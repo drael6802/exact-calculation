92efd5646227d96fa0ac5efa7dcff27e9011dcb6
drael6802 <drael@naver.com>
2025-01-11 15:47:11 +0900

    build

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -1,33 +1,45 @@
 function ResultSection({ result }) {
+	// result가 없거나 빈 객체인 경우 체크
+	const isEmpty = !result || Object.keys(result).length === 0;
+
 	return (
 		<div className="result-section">
 			<h5>정산 결과</h5>
-			<ul>
-				{/* 각 멤버별 정산 결과 */}
-				{Object.entries(result).map(([memberName, data], index) => (
+			{/* 결과가 없는 경우 스켈레톤 추가 */}
+			{isEmpty ? (
+				<ul>
+					<li className="result-item">
+						<div className="member-summary"><strong></strong> <span className="total-payout"></span></div>
+						<ul className="menu-details"></ul>
+					</li>
+				</ul>
+			) : (
+				<ul>
+					{/* 각 멤버별 정산 결과 */} {Object.entries(result).map(([memberName, data], index) => (
 					<li key={index} className="result-item">
 						<div className="member-summary">
-							<strong>{memberName}</strong> {/* 멤버 이름 */}
-							<span className="total-payout">
-								총 정산금: {new Intl.NumberFormat('ko-KR').format(Math.ceil(data.payout))}원
-							</span>
-						</div>
-						{/* 메뉴별 상세 분배 내역 */}
-						<ul className="menu-details">
-							{data.menusPay.map((menu, idx) => (
-								<li key={idx} className="menu-pay-item">
-									{/* 메뉴별 이름과 금액 출력 */}
-									{Object.entries(menu).map(([menuName, amount]) => (
-										<span key={menuName} className="menu-pay-detail">
-											<strong>{menuName}</strong>: {new Intl.NumberFormat('ko-KR').format(Math.ceil(amount))}원
-										</span>
-									))}
-								</li>
-							))}
-						</ul>
-					</li>
-				))}
-			</ul>
+								<strong>{memberName}</strong> {/* 멤버 이름 */}
+								<span className="total-payout">
+									총 정산금: {new Intl.NumberFormat('ko-KR').format(Math.ceil(data.payout))}원
+								</span>
+							</div>
+							{/* 메뉴별 상세 분배 내역 */}
+							<ul className="menu-details">
+								{data.menusPay.map((menu, idx) => (
+									<li key={idx} className="menu-pay-item">
+										{/* 메뉴별 이름과 금액 출력 */}
+										{Object.entries(menu).map(([menuName, amount]) => (
+											<span key={menuName} className="menu-pay-detail">
+												<strong>{menuName}</strong>: {new Intl.NumberFormat('ko-KR').format(Math.ceil(amount))}원
+											</span>
+										))}
+									</li>
+								))}
+							</ul>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 }
